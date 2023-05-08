338c72a19209ce0f729b76c556e2f649bb2aabd6
Guilherme Serafim Kollet <sweatkollet@gmail.com>
2023-05-07 22:12:44 -0300

    update lerAudioVisuais() from Acervo.java

--- src/Acervo.java
+++ src/Acervo.java
@@ -3,12 +3,15 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.Serializable;
+import java.lang.reflect.Array;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class Acervo {
+    ArrayList<AudioVisual> audiovisuais = new ArrayList<AudioVisual>();
 
     public Acervo() {
         System.out.println("Criando acervo...");
@@ -20,7 +23,8 @@ public class Acervo {
     */
     public void lerAudioVisuais() {
         System.out.println("Carregando audiovisuais...");
-
+        
+        //teste
         AudioVisual a1 = new Game("GTA V", 100.0, "ACAO");
         AudioVisual a2 = new BluRay("O Poderoso Chefão", 50.0, 180);
 
@@ -50,11 +54,13 @@ public class Acervo {
                 if(console[2].equals("1")) {
                     a = new BluRay(console[0], Double.parseDouble(console[1]), Integer.parseInt(console[3]));
                     System.out.println("BluRay criado com sucesso!");
+                    audiovisuais.add(a);
                     itensCarregados++;
                 }
                 else if(console[2].equals("2")){
                     a = new Game(console[0], Double.parseDouble(console[1]), console[3]);
                     System.out.println("Game criado com sucesso!");
+                    audiovisuais.add(a);
                     itensCarregados++;
                 } 
                 else {
@@ -66,6 +72,17 @@ public class Acervo {
             System.out.println("Quantidade de itens carregados: " + itensCarregados);
             System.out.println("Quantidade de itens não carregados: " + itensNaoCarregados);
             
+            try {
+                FileWriter writer = new FileWriter("D:\\Development\\Java Learn\\TP2\\App\\data\\resultado.csv");
+                writer.write("1," + itensCarregados);
+                writer.close();
+                System.out.println("Dados escritos com sucesso no arquivo.");
+                
+            } catch (IOException e) {
+                System.out.println("Erro ao escrever no arquivo.");
+                e.printStackTrace();
+            }
+
         }
         catch (IOException e) {
             System.err.format("Erro de E/S: %s%n", e);
