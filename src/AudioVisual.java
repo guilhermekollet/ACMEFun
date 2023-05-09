554acd5f71b67279b25b86a3c49614a675f77859
Guilherme Serafim Kollet <sweatkollet@gmail.com>
2023-05-08 21:56:21 -0300

    repaired

--- src/AudioVisual.java
+++ src/AudioVisual.java
@@ -2,6 +2,11 @@ abstract class AudioVisual implements Cobravel {
     private String titulo;
     private double precoBase;
 
+    AudioVisual(String titulo, double precoBase) {
+        this.titulo = titulo;
+        this.precoBase = precoBase;
+    }
+    
     @Override
     public double calculaPrecoVenda() {
         
@@ -16,6 +21,18 @@ abstract class AudioVisual implements Cobravel {
 
     public String getTitulo() {
         return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public double getPrecoBase() {
+        return precoBase;
+    }
+
+    public void setPrecoBase(double precoBase) {
+        this.precoBase = precoBase;
     }
 
     
